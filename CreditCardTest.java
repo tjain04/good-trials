6e8e35664c9b09ffec70d79a9fd9022bb1d746b8
tjain04 <40927725+tjain04@users.noreply.github.com>
2018-07-08 16:39:22 +0530

    Update CreditCardTest.java

--- CreditCardTest.java
+++ CreditCardTest.java
@@ -1,24 +1,49 @@
-package com.fis.service;
+package com.jwt.rest;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
 
 public class CreditCardTest {
-	CreditCardEngine calculator;
+	CreditCardEngine engine;
+	List<CreditCardDTO> list;
 		@Before
 		public void setUp() {
-			CreditCardEngine calculator = new CreditCardEngine();	
+			engine = new CreditCardEngine();
+			list = new ArrayList<CreditCardDTO>();
 		}
 		
 		@Test
-		public void testCreditCardArgs() {			
-			Boolean success = calculator.startCreditCardEngine(null, null);
-			assertFalse(success);
+		public void testCreditCardInvalidArgs() {			
+			list = engine.startCreditCardEngine(null, null);
+			assertTrue(list.isEmpty());
 		}
+		
+		public void testVISACreditCard() {			
+			list = engine.startCreditCardEngine("VISA", 2);
+			assertTrue(list.size() == 2);
+		}
+		
+		public void testAmexCardCreditCard() {			
+			list = engine.startCreditCardEngine("AMEX", 4);
+			assertTrue(list.size() == 4);
+			assertTrue(list.get(0).getCardNumber().startsWith("3"));
+		}
+		
+		public void testDiscoverCreditCard() {			
+			list = engine.startCreditCardEngine("DISCOVER", 2);
+			assertTrue(list.size() == 2);
+			assertTrue(list.get(1).getCardNumber().startsWith("6"));
+		}
+		
+		public void testMasterCreditCard() {			
+			list = engine.startCreditCardEngine("MASTERCARD", 2);
+			assertTrue(list.size() == 2);
+		}
+
 
 }
